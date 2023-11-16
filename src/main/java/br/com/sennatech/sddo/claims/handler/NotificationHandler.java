e3dc41cae57ed1519edd1c4c93567cf66e3849c0
Leonardo Bezerra <leonardo.bispo@sennatech.com>
2023-11-16 09:43:13 -0300

    consertando envio de evento na criação de notificação

--- src/main/java/br/com/sennatech/sddo/claims/handler/NotificationHandler.java
+++ src/main/java/br/com/sennatech/sddo/claims/handler/NotificationHandler.java
@@ -10,6 +10,7 @@ import br.com.sennatech.sddo.claims.config.Config;
 import br.com.sennatech.sddo.claims.domain.dto.ClaimDTO;
 import br.com.sennatech.sddo.claims.domain.dto.event.EventDTO;
 import br.com.sennatech.sddo.claims.domain.dto.util.ResponseDTO;
+import br.com.sennatech.sddo.claims.domain.entity.Claim;
 import br.com.sennatech.sddo.claims.service.ClaimService;
 import br.com.sennatech.sddo.claims.util.LoggerUtil;
 import jakarta.persistence.EntityNotFoundException;
@@ -25,7 +26,7 @@ public class NotificationHandler {
   @FunctionName("create-notification")
   public HttpResponseMessage run(
       @HttpTrigger(name = "req", methods = {
-          HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS, route = "notification") HttpRequestMessage<String> request,
+          HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS, route = "notification") HttpRequestMessage<String> request,
       @EventHubOutput(name = "event", eventHubName = Config.EVENT_HUB_NAME, connection = Config.CONN_STRING) OutputBinding<String> outputItem,
       final ExecutionContext context) {
 
@@ -34,11 +35,12 @@ public class NotificationHandler {
 
     try {
       ClaimDTO claimDTO = mapper.readValue(request.getBody(), ClaimDTO.class);
-      service.create(claimDTO);
-      String event = mapper.writeValueAsString(EventDTO.create(context, claimDTO));
+      Claim claim = service.create(claimDTO);
+      String event = mapper.writeValueAsString(EventDTO.create(context, claim));
       outputItem.setValue(event);
       Object refusalReasons = (service.getAutoRefusalReasons().isEmpty()) ? null : service.getAutoRefusalReasons();
-      if (refusalReasons != null) logger.info("Auto refusal reasons: " + refusalReasons);
+      if (refusalReasons != null)
+        logger.info("Auto refusal reasons: " + refusalReasons);
       return request.createResponseBuilder(HttpStatus.CREATED).build();
     } catch (EntityNotFoundException e) {
       logger.logError(e);
--- src/main/java/br/com/sennatech/sddo/claims/service/ClaimService.java
+++ src/main/java/br/com/sennatech/sddo/claims/service/ClaimService.java
@@ -39,13 +39,14 @@ public class ClaimService {
         return autoRefusalReasons;
     }
 
-    public void create(ClaimDTO claimDTO) {
+    public Claim create(ClaimDTO claimDTO) {
         Claim claim = claimDTOtoClaim.apply(claimDTO);
         Notifier notifier = notifierService.retrieveOrCreateNotifier(claim.getNotifier().getDocumentNumber(),
                 claimDTO.getNotifier());
         claim.setNotifier(notifier);
         checkConstraints(claim);
         claimRepository.saveAndFlush(claim);
+        return claim;
     }
 
     public ClaimDetailsDTO retrieveFromClaimId(String claimId) {
